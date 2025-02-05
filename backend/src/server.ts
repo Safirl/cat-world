300099d007d53f979853c266ad9ea206127f8a45
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-05 14:20:45 +0100

    init project

---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import mongod from 'mongod';
+import db from './config/db';
+import connectDB from './config/db';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to the backend server");
+});
+
+connectDB()
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err: any) => {
+        console.error(err);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
