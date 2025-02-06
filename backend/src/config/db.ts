808bb52743a5734ac5784503ee537ccfb64a4b1c
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-06 14:41:47 +0100

    cleaning logs

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,7 +6,6 @@ dotenv.config();
 const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string);
-        console.log("MongoDB connecté");
     } catch (error) {
         console.error("Erreur de connexion à MongoDB", error);
         process.exit(1);
--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -10,7 +10,6 @@ class AuthController {
             const { username, email, password } = req.body;
 
             const hashedPassword = await bcrypt.hash(password, 10);
-            console.log(hashedPassword);
             const newUser = new User({
                 username,
                 email,
