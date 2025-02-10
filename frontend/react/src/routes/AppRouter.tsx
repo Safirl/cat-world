619805119b537ce94db31b329ffb270099155859
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-10 10:57:52 +0100

    adding router

---
+++ frontend/react/src/routes/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from '../pages/Home';
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                {/* <Route path="*" element={<NotFoundPage />} /> */}
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
