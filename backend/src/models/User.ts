c2078d81a5334f6eee50225d5ca8998bbcf6f2d0
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-16 23:22:41 +0100

    adding user and friend seeder

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -7,13 +7,15 @@ export interface IUser extends Document {
     email: string;
     password: string;
     color: string
+    isAdmin: boolean
 }
 
 const UserSchema: Schema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    color: { type: String, require: false }
+    color: { type: String, require: false },
+    isAdmin: { type: Boolean, require: false }
 }, { timestamps: true, collection: 'users' });
 
 type PreDeleteQueryMiddleware = Query<any, IUser> & {
@@ -24,11 +26,19 @@ UserSchema.pre("findOneAndDelete", async function (this: PreDeleteQueryMiddlewar
     try {
         const userId = this._conditions._id;
         // Suppression des entrées UserLetter associées
-        await UserLetter.deleteMany({ sender_id: userId });
-        await UserLetter.deleteMany({ receiver_id: userId });
+        await UserLetter.deleteMany({
+            $or: [
+                { sender_id: userId },
+                { receiver_id: userId }
+            ]
+        });
 
-        await Friend.deleteMany({ user_id_1: userId })
-        await Friend.deleteMany({ user_id_2: userId })
+        await Friend.deleteMany({
+            $or: [
+                { user_id_1: userId },
+                { user_id_2: userId }
+            ]
+        })
 
         next();
     } catch (error) {
