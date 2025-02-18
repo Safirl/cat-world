8eb44d19a0608e9d6919077c7fc6655641bba7e9
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-18 19:27:16 +0100

    fixing names

--- frontend/src/pages/components/Navbar.tsx
+++ frontend/src/pages/components/Navbar.tsx
@@ -4,40 +4,40 @@ type NavBar = {
 
 };
 
-const NavBar: React.FC<NavBar> = ({  }) => {
+const NavBar: React.FC<NavBar> = ({ }) => {
     return (
-            <nav>
-                <div className="buttonNavbar">
-                    <div className="left hylightLabelButtonBackground">
-                        <div className="label">
-                            <img src="/image/navbar/planet.svg" alt="" />
-                            <p>Ma planète</p>
-                        </div>
-                        <img className="hylightCote hylightCoteleft" src="/image/navbar/hylightRight.svg" alt="" />
-                        <img className="hylight " src="/image/navbar/hylightBottom.svg" alt="" />
-                        <img className="hylightUnder " src="/image/navbar/hylightunder.svg" alt="" />
-                        
+        <nav>
+            <div className="buttonNavbar">
+                <div className="left highlightLabelButtonBackground">
+                    <div className="label">
+                        <img src="/image/navbar/planet.svg" alt="" />
+                        <p>Ma planète</p>
                     </div>
-                    <div className="center hylightLabelButtonBackground">
-                        <div className="label">
-                                <img src="/image/navbar/letter.svg" alt="" />
-                                <p>Mes lettres</p>
-                        </div>
-                        <img className="hylight center" src="/image/navbar/hylightBottom.svg" alt="" />
-                        <img className="hylightUnder " src="/image/navbar/hylightundercenter.svg" alt="" />
-                    </div>
-                    <div className="right hylightLabelButtonBackground">
-                        <div className="label">
-                                <img src="/image/navbar/account.svg" alt="" />
-                                <p>Mon compte</p>
-                        </div>
-                        <img className="hylightCote hylightCoteright" src="/image/navbar/hylightLeft.svg" alt="" />
-                        <img className="hylight " src="/image/navbar/hylightBottom.svg" alt="" />
-                        <img className="hylightUnder " src="/image/navbar/hylighundertright.svg" alt="" />
-                    </div>
+                    <img className="highlightCote highlightCoteleft" src="/image/navbar/highlightRight.svg" alt="" />
+                    <img className="highlight " src="/image/navbar/highlightBottom.svg" alt="" />
+                    <img className="highlightUnder " src="/image/navbar/highlightunder.svg" alt="" />
+
                 </div>
-                <img className="background" src="/image/navbar/background.svg" alt="" />
-            </nav>
+                <div className="center highlightLabelButtonBackground">
+                    <div className="label">
+                        <img src="/image/navbar/letter.svg" alt="" />
+                        <p>Mes lettres</p>
+                    </div>
+                    <img className="highlight center" src="/image/navbar/highlightBottom.svg" alt="" />
+                    <img className="highlightUnder " src="/image/navbar/highlightundercenter.svg" alt="" />
+                </div>
+                <div className="right highlightLabelButtonBackground">
+                    <div className="label">
+                        <img src="/image/navbar/account.svg" alt="" />
+                        <p>Mon compte</p>
+                    </div>
+                    <img className="highlightCote highlightCoteright" src="/image/navbar/highlightLeft.svg" alt="" />
+                    <img className="highlight " src="/image/navbar/highlightBottom.svg" alt="" />
+                    <img className="highlightUnder " src="/image/navbar/highlightundertright.svg" alt="" />
+                </div>
+            </div>
+            <img className="background" src="/image/navbar/background.svg" alt="" />
+        </nav>
     );
 };
 
