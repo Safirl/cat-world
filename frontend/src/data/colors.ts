1598d9f04f8469a486e74dd5b76b75fd3860d4a9
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-20 17:16:55 +0100

    adding customize cat

---
+++ frontend/src/data/colors.ts
@@ -0,0 +1,14 @@
+const colors = [
+    {
+        texture: "cat_texture_black.png",
+        primary: "#150906",
+        secondary: "#EEB178",
+    },
+    {
+        texture: "cat_texture_white.png",
+        primary: "#E5C8C3",
+        secondary: "#95A8F3",
+    },
+]
+
+export default colors;
