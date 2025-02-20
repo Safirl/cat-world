26d5984bddcae10e5dc24a3fceafbdb56a10e77f
Safirl <loic.leforestier@mmibordeaux.com>
2025-02-20 15:03:12 +0100

    added stack of letters

--- frontend/src/pages/Letters.tsx
+++ frontend/src/pages/Letters.tsx
@@ -9,6 +9,8 @@ const Letters = () => {
     const navigate = useNavigate();
     const [unreadLetters, setUnreadLetters] = useState<{ _id: string, title: string, content: string, stamp: string, src_img: string, sender_id: string, createdAt: string }[]>()
     const [readLetters, setReadLetters] = useState<{ _id: string, title: string, content: string, stamp: string, src_img: string, sender_id: string, createdAt: string }[]>()
+    const [readNumber, setReadNumber] = useState(0)
+    const [unreadNumber, setUnreadNumber] = useState(0)
 
     const handleCreateLetter = () => {
         navigate(routes.createLetter);
@@ -35,19 +37,35 @@ const Letters = () => {
             );
 
             const data = await response.json();
-            console.log(data)
             if (!response.ok) {
                 console.error(data.message);
+                return;
             }
             setUnreadLetters(data.letters);
+            const count: number = data.letters.length;
+            console.log(count)
+            if (count < 1) {
+                setUnreadNumber(1);
+            } else if (count < 5) {
+                setUnreadNumber(1);
+            } else if (count < 10) {
+                setReadNumber(5);
+            } else if (count < 15) {
+                setReadNumber(10);
+            } else if (count < 25) {
+                setReadNumber(15);
+            } else {
+                setReadNumber(25);
+            }
 
         } catch (error) {
             console.error("Error while submiting request: ", error);
         }
     }
+
     const fetchreadLetters = async () => {
         try {
-            
+
             const response = await fetch(
                 import.meta.env.VITE_API_URL + apiRoutes.fetchLetterRead,
                 {
@@ -58,14 +76,28 @@ const Letters = () => {
                     credentials: "include",
                 }
             );
-            console.log('response', response)
 
             const data = await response.json();
-            console.log(data)
             if (!response.ok) {
                 console.error(data.message);
+                return;
             }
-            setUnreadLetters(data.letters);
+            setReadLetters(data.letters);
+            const count: number = data.letters.length;
+            if (count < 1) {
+                setReadNumber(0);
+            } else if (count < 5) {
+                setReadNumber(1);
+            } else if (count < 10) {
+                setReadNumber(5);
+            } else if (count < 15) {
+                setReadNumber(10);
+            } else if (count < 25) {
+                setReadNumber(15);
+            } else {
+                setReadNumber(25);
+            }
+
 
         } catch (error) {
             console.error("Error while submiting request: ", error);
@@ -79,7 +111,6 @@ const Letters = () => {
 
     return (
         <>
-
             <div className="containerLetter">
                 <ButtonRound
                     text="Envoyer une lettre"
@@ -97,24 +128,22 @@ const Letters = () => {
                 </div>
 
                 <div className="stackletter">
-                    <img className="stack read" src="/image/letters/stackOfletters25.svg" alt="Tas de lettre  lu" />
-                    <img className="stack unread" src="/image/letters/stackOfletters25.svg" alt="Tas de lettre non lu" />
-                    <img className="shadowRead" src="/image/decors/shadowLeft.svg" alt="ombre lettre lu" />
-                    <img className="shadowUnread" src="/image/decors/shadow.svg" alt="ombre lettre non lu" />
+                    {readNumber > 0 && <img className="stack read" src={`/image/letters/stackOfletters${readNumber}.svg`} alt="Tas de lettre lu" />}
+                    {unreadNumber > 1 && <img className="stack unread" src={`/image/letters/stackOfletters${unreadNumber}.svg`} alt="Tas de lettre non lu" />}
+                    {readNumber > 5 && <img className="shadowRead" src="/image/decors/shadowLeft.svg" alt="ombre lettre lu" />}
+                    {unreadNumber > 5 && <img className="shadowUnread" src="/image/decors/shadow.svg" alt="ombre lettre non lu" />}
                 </div>
 
                 <div className="letter">
+                    {
+                        unreadLetters && unreadLetters.length > 0 ?
+                            <div onClick={handleShowLetter}>
 
-                {
-                    unreadLetters && unreadLetters.length > 0 ?
-                        <div onClick={handleShowLetter}>
-                            
-                            <img className="elementLetter" src="/image/letters/letterPerspective.svg" alt="texture png" />
-                        </div>
-                        :
-                        <div><img className="elementLetter" src="/image/decors/postItNoletter.svg" alt="texture png" /></div>
-                }
-                
+                                <img className="elementLetter" src="/image/letters/letterPerspective.svg" alt="texture png" />
+                            </div>
+                            :
+                            <div><img className="elementLetter" src="/image/decors/postItNoletter.svg" alt="texture png" /></div>
+                    }
                 </div>
             </div>
         </>
