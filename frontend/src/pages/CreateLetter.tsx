fe8189cc6bc8b3d86f5f436733f675754d2daa5a
Safirl <loic.leforestier@mmibordeaux.com>
2025-06-07 18:29:49 +0200

    update

--- frontend/src/pages/CreateLetter.tsx
+++ frontend/src/pages/CreateLetter.tsx
@@ -173,85 +173,83 @@ const CreateLetter = () => {
 
     return (
         <>
-            <div className="createLetterPage">
-                <div className="createLetterContainer">
-                    <h1>Ma lettre</h1>
-                    <div className="letter">
-                        {message && <p>{message}</p>}
-                        <img className="letterBackground" src="/image/letters/letter-background.svg" alt="letter background" />
+            <div className="createLetterContainer">
+                <h1>Ma lettre</h1>
+                <div className="letter">
+                    {message && <p>{message}</p>}
+                    <img className="letterBackground" src="/image/letters/letter-background.svg" alt="letter background" />
 
-                        <form className="letterContentForm" onSubmit={handleSubmit}>
-                            <div>
-                                <div className="letterHeaderCreate">
-                                    <button className="buttonStamp" type="button" onClick={() => setShowStamps(!showStamps)}>
-                                        {formData.stamp && formData.stamp !== "test" ? (
-                                            <img
-                                                className="stampImage"
-                                                src={`/image/stamps/${formData.stamp}`}
-                                                alt="chosen stamp"
-                                            />
-                                        ) : (
-                                            <>
-                                                <img src="/image/icons/import.svg" />
-                                                <p>Ajouter un timbre</p>
-                                            </>
-                                        )}
-                                    </button>
-                                    <div className="letterInformation">
-                                        <p className="username">De : {user?.username}</p>
-                                        <div className="selectReceiver">
-                                            <p className="receiver"> À :  </p>
-                                            <select name="receiver_id" id="receiver-select" onChange={handleChange} value={formData.receiver_id} required>
-                                                <option value="">Choisis un(e) ami(e)</option>
-                                                {friends && friends.map(friend => (
-                                                    <option key={friend._id} value={friend._id}>{friend.username}</option>
-                                                ))}
-                                            </select>
-                                        </div>
-                                        <p className="date">Le : {formattedDate}</p>
+                    <form className="letterContentForm" onSubmit={handleSubmit}>
+                        <div>
+                            <div className="letterHeaderCreate">
+                                <button className="buttonStamp" type="button" onClick={() => setShowStamps(!showStamps)}>
+                                    {formData.stamp && formData.stamp !== "test" ? (
+                                        <img
+                                            className="stampImage"
+                                            src={`/image/stamps/${formData.stamp}`}
+                                            alt="chosen stamp"
+                                        />
+                                    ) : (
+                                        <>
+                                            <img src="/image/icons/import.svg" />
+                                            <p>Ajouter un timbre</p>
+                                        </>
+                                    )}
+                                </button>
+                                <div className="letterInformation">
+                                    <p className="username">De : {user?.username}</p>
+                                    <div className="selectReceiver">
+                                        <p className="receiver"> À :  </p>
+                                        <select name="receiver_id" id="receiver-select" onChange={handleChange} value={formData.receiver_id} required>
+                                            <option value="">Choisis un(e) ami(e)</option>
+                                            {friends && friends.map(friend => (
+                                                <option key={friend._id} value={friend._id}>{friend.username}</option>
+                                            ))}
+                                        </select>
                                     </div>
-                                </div>
-                                {hasSubmitted && stampError && (
-                                    <p className="error-message" style={{ color: 'red', fontSize: '0.8rem', marginTop: '4px' }}>
-                                        {stampError}
-                                    </p>
-                                )}
-                            </div>
-                            <div className="letterBody">
-                                <div>
-                                    <label htmlFor="title">Titre</label>
-                                    <input id="title" type='text' name="title" placeholder="Ma lettre" onChange={handleChange} value={formData.title} required />
-                                </div>
-                                <div className="letterContainerContent">
-                                    <label htmlFor="content">Contenu</label>
-                                    <textarea className="letterContentText" id="content" name="content" placeholder="Cher ami..." onChange={handleChange} value={formData.content} required />
+                                    <p className="date">Le : {formattedDate}</p>
                                 </div>
                             </div>
-                            <p className="senderText">{user?.username}</p>
-                            <div className="sendContainer">
-                                <div className="addImageContainer">
-                                    <label htmlFor="image">
-                                        {
-                                            image ?
-                                                <img className="imageInput" src={URL.createObjectURL(image)} alt="letter selected image" />
-                                                :
-                                                <div className="imageInput">
-                                                    <img className="importImage" src="/image/icons/import.svg" />
-                                                    <p>Ajouter une image</p>
-                                                </div>
-                                        }
-                                    </label>
-                                    <input style={{ display: "none" }} placeholder="mon image" title="choisir une image" id="image" type='file' name="image" accept="image/*" onChange={handleChange} />
-                                </div>
-                                <button className="sendButton" type="submit" disabled={hasSubmitted}><p>Envoyer</p></button>
+                            {hasSubmitted && stampError && (
+                                <p className="error-message" style={{ color: 'red', fontSize: '0.8rem', marginTop: '4px' }}>
+                                    {stampError}
+                                </p>
+                            )}
+                        </div>
+                        <div className="letterBody">
+                            <div>
+                                <label htmlFor="title">Titre</label>
+                                <input id="title" type='text' name="title" placeholder="Ma lettre" onChange={handleChange} value={formData.title} required />
                             </div>
-                        </form>
-                    </div>
+                            <div className="letterContainerContent">
+                                <label htmlFor="content">Contenu</label>
+                                <textarea className="letterContentText" id="content" name="content" placeholder="Cher ami..." onChange={handleChange} value={formData.content} required />
+                            </div>
+                        </div>
+                        <p className="senderText">{user?.username}</p>
+                        <div className="sendContainer">
+                            <div className="addImageContainer">
+                                <label htmlFor="image">
+                                    {
+                                        image ?
+                                            <img className="imageInput" src={URL.createObjectURL(image)} alt="letter selected image" />
+                                            :
+                                            <div className="imageInput">
+                                                <img className="importImage" src="/image/icons/import.svg" />
+                                                <p>Ajouter une image</p>
+                                            </div>
+                                    }
+                                </label>
+                                <input style={{ display: "none" }} placeholder="mon image" title="choisir une image" id="image" type='file' name="image" accept="image/*" onChange={handleChange} />
+                            </div>
+                            <button className="sendButton" type="submit" disabled={hasSubmitted}><p>Envoyer</p></button>
+                        </div>
+                    </form>
                 </div>
-                {showStamps && <StampList />}
-
-
             </div>
+            {showStamps && <StampList />}
+
+
             <div className={`validationSendAnimation ${showValidation ? 'visible' : ''}`}>
                 <p>Lettre envoyée</p>
                 <img src="/image/letters/letter.svg" alt="" />
